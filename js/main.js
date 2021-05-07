748aefacefa9a9ce465051c270f089b1e8f806be
RetryMaster <70057581+RetryMaster@users.noreply.github.com>
2021-05-07 18:13:50 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,9 +4,17 @@ $(document).ready(function(){
     $('.burger').on('click', function(event){
         $('.burger').toggleClass('active');
         $('.menu').toggleClass('active');
-        $('body').toggleClass('.lock');
+        $('body').toggleClass('lock');
     });
     $('.header_menu-element').on('click', function(){
         $('.menu , .burger').removeClass('active');
     });
+    $('.offer_btn').on('click' , function(){
+        $('.call_wrap').addClass('current');
+        $('body').addClass('lock');
+    });
+    $('.close').on('click',function(){ 
+        $('.call_wrap').removeClass('current');
+        $('body').removeClass('lock');
+    }); 
 });
